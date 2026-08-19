cc03116c83d36b369840da034d874ca7a55a6e5f
danikmarukovitch <marukovitch3@icloud.com>
2026-08-19 20:30:27 +0200

    create API Gateway with WebFlux routing

---
+++ src/test/java/org/example/internship_api_gateway/InternshipApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.internship_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
