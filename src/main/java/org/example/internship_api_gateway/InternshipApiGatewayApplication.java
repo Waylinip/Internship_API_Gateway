dd1dd9733f3db800a48cfb0163d7d852441f1253
danikmarukovitch <marukovitch3@icloud.com>
2026-08-19 21:44:25 +0200

    add JWT security filter

---
+++ src/main/java/org/example/internship_api_gateway/InternshipApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.internship_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipApiGatewayApplication.class, args);
+    }
+
+}
